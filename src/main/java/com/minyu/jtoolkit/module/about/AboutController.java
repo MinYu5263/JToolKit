4909114ef2524a01b134f806f1cfcd672df20d93
minyu <minyu623@aliyun.com>
2025-12-29 21:34:48 +0800

    新增页面：关于

---
+++ src/main/java/com/minyu/jtoolkit/module/about/AboutController.java
@@ -0,0 +1,54 @@
+package com.minyu.jtoolkit.module.about;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.net.URI;
+import java.util.Objects;
+
+@Component
+public class AboutController {
+
+    @FXML private VBox root;
+    @FXML private ImageView appLogo;
+    @FXML private Label versionLabel;
+    @FXML private Label systemInfoLabel;
+
+    @FXML
+    public void initialize() {
+        try {
+            appLogo.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/logo_128.png"))));
+            appLogo.setFitWidth(64);
+            appLogo.setFitHeight(64);
+        } catch (Exception ignored) {}
+
+        versionLabel.setText("Version 1.0.0-SNAPSHOT");
+
+        String javaVersion = System.getProperty("java.version");
+        String osName = System.getProperty("os.name");
+        systemInfoLabel.setText("Running on Java " + javaVersion + " (" + osName + ")");
+    }
+
+    @FXML
+    private void openGithub() {
+        openLink("https://github.com/MinYu5263/JToolKit");
+    }
+
+    @FXML
+    private void openWebsite() {
+        openLink("https://github.com/MinYu5263");
+    }
+
+    private void openLink(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
