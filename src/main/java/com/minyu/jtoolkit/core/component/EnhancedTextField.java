b9514acf82081862f0e24739fac6a33b9850dddf
minyu <minyu5263@gmail.com>
2026-01-05 16:52:19 +0800

    改进自定义组件效果

--- src/main/java/com/minyu/jtoolkit/core/component/EnhancedTextField.java
+++ src/main/java/com/minyu/jtoolkit/core/component/EnhancedTextField.java
@@ -1,11 +1,13 @@
 package com.minyu.jtoolkit.core.component;
 
-import javafx.scene.control.TextField;
+import atlantafx.base.controls.CustomTextField;
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.Node;
 
 /**
  * EnhancedTextField
  */
-public class EnhancedTextField extends EnhancedInputBase<TextField> {
+public class EnhancedTextField extends EnhancedInputBase<CustomTextField> {
 
     public EnhancedTextField() {
         super();
@@ -13,7 +15,32 @@ public class EnhancedTextField extends EnhancedInputBase<TextField> {
     }
 
     @Override
-    protected TextField createInputControl() {
-        return new TextField();
+    protected CustomTextField createInputControl() {
+        return new CustomTextField();
+    }
+
+    // 代理方法
+    public final ObjectProperty<Node> leftProperty() {
+        return inputControl.leftProperty();
+    }
+
+    public final Node getLeft() {
+        return inputControl.getLeft();
+    }
+
+    public final void setLeft(Node node) {
+        inputControl.setLeft(node);
+    }
+
+    public final ObjectProperty<Node> rightProperty() {
+        return inputControl.rightProperty();
+    }
+
+    public final Node getRight() {
+        return inputControl.getRight();
+    }
+
+    public final void setRight(Node node) {
+        inputControl.setRight(node);
     }
 }
